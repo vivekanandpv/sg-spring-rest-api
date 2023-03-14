3fe3178a59aee258bc3ad913582ca24b45851bb3
vivek <vivekanandpv@outlook.com>
2023-03-14 11:42:48 +0530

    xml-serialization and deserialization

--- src/main/java/com/example/demo/apis/CarApi.java
+++ src/main/java/com/example/demo/apis/CarApi.java
@@ -1,13 +1,16 @@
 package com.example.demo.apis;
 
 import com.example.demo.models.Book;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.print.attribute.standard.Media;
 
 @RestController
 @RequestMapping("api/v1/cars")
 public class CarApi {
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
     public ResponseEntity<Book> get(@RequestParam(required = false) String title) {
         Book book = new Book();
         book.setTitle(title == null ? "Car Default Title" : title);
@@ -16,6 +19,11 @@ public class CarApi {
         book.setPages(1200);
         book.setPrice(2000.25);
 
+        return ResponseEntity.ok(book);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_XML_VALUE, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public ResponseEntity<Book> create(@RequestBody Book book) {
         return ResponseEntity.ok(book);
     }
 }
